47d20dc04f302f51902be09c666999dc9a87d92c
bman <jtrain184@gmail.com>
2018-10-06 15:14:00 -0700

    Finished transpose.sh

--- transpose/transpose.sh
+++ transpose/transpose.sh
@@ -1,1 +1,17 @@
-awk file.txt
+awk '
+    BEGIN { FS=" " } 
+        {for(i=1; i<=NF; ++i)
+            { 
+                # If empty, store first word
+                if(word[i] == "")
+                    { word[i] = $i }
+                else
+                    { word[i] = word[i]" "$i}
+            }
+        }
+        END {  
+                {for(i=1; i<=NF; ++i)
+                    { print word[i] }
+                } 
+            }
+' file.txt
